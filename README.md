fd7efdd5391d500ce956f3d8625a796e95de1b11
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-06-15 11:19:15 +0200

    Added README.md, pyproject.toml and renamed main.py to pybind_example.py.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example Project showing how to Create a Python Package with a pybind11 backend
+
+This example project uses pybind11 as a submodule add to `external/pybind11`.
--- pybind_example/pybind_example.py
+++ pybind_example/pybind_example.py
@@ -1,5 +1,10 @@
 from .backend.pybind_example import TestClass
 
-if __name__ == "__main__":
+
+def run():
     tc = TestClass(42)
     print(f"{tc.get_num()}")
+
+
+if __name__ == "__main__":
+    run()
