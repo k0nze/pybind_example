8f2965261e3facf7b9caa76dc0a161fa1c5e25a3
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2023-06-19 15:17:01 +0200

    Fixed signature in init of wrapper.

--- src/backend/wrapper.cc
+++ src/backend/wrapper.cc
@@ -1,5 +1,7 @@
 #include <pybind11/pybind11.h>
 
+#include <string>
+
 #include "animal.h"
 #include "cat.h"
 #include "dog.h"
@@ -8,17 +10,17 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(pybind_example, m) {
     py::class_<Animal>(m, "Animal")
-        .def(py::init<char* const>(), py::arg("name"))
+        .def(py::init<const std::string&>(), py::arg("name"))
         .def("get_name", &Animal::get_name)
         .def("make_sound", &Animal::make_sound);
 
     py::class_<Dog>(m, "Dog")
-        .def(py::init<char* const>(), py::arg("name"))
+        .def(py::init<const std::string&>(), py::arg("name"))
         .def("wag_tail", &Dog::wag_tail)
         .def("make_sound", &Dog::make_sound);
 
     py::class_<Cat>(m, "Cat")
-        .def(py::init<char* const>(), py::arg("name"))
+        .def(py::init<const std::string&>(), py::arg("name"))
         .def("purr", &Cat::purr)
         .def("make_sound", &Cat::make_sound);
 }
